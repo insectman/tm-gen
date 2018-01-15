34a793b97fe7af22ea5c92df9f35abdf6856f2ac
insectman <insectman@yandex.ru>
2018-01-15 15:43:37 +0300

    add board generation

---
+++ index.js
@@ -0,0 +1,65 @@
+var http = require('http'); //add the http module
+var fs = require('fs'); //add the http module
+
+var spriteFile;
+var cssFile;
+
+const filesMap = new Map();
+
+['./images/awAchSprite.png',
+  './images/board.jpg',
+  './images/oceanTile.png',
+  './images/resSprite.png',
+  './main.js',
+  './board.html',
+  './index.html',
+  './styles/main.css'].forEach(fileName =>
+    fs.readFile(fileName, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      filesMap.set(fileName, data);
+    }));
+
+var myServer = http.createServer(function (request, response) {
+  var awards, achievements;
+
+  let source = filesMap.get(`.${request.url}`);
+
+  switch (request.url) {
+    case "/main.js":
+    response.writeHead(200, { "Content-Type": "application/javascript" });
+      break;
+    case "/styles/main.css":
+      response.writeHead(200, { "Content-Type": "text/css" });
+      break;
+    case "/images/board.jpg":
+    case "/images/awAchSprite.png":
+    case "/images/resSprite.png":
+    case "/images/oceanTile.png":
+      if (!response.getHeader('Cache-Control') || !response.getHeader('Expires')) {
+        //response.setHeader("Cache-Control", "public, max-age=345600"); // ex. 4 days in seconds.
+        response.setHeader("Expires", new Date(Date.now() + 345600000).toUTCString());  // in ms.
+      }
+      if(request.url == "/images/board.jpg") {
+        response.writeHead(200, { "Content-Type": "image/jpeg" });
+      }
+      else {
+        response.writeHead(200, { "Content-Type": "image/png" });
+      }
+      break;
+    case '/board':
+      response.writeHead(200, { "Content-Type": "text/html" });
+      source = filesMap.get('./board.html');
+      break;
+    default:
+      response.writeHead(200, { "Content-Type": "text/html" });
+      source = filesMap.get('./index.html');
+  };
+  
+  response.end(source);
+});
+
+var port = process.env.PORT || 3000
+
+myServer.listen(port);
--- main.js
+++ main.js
@@ -1,63 +1,3 @@
-var http = require('http'); //add the http module
-var fs = require('fs'); //add the http module
-
-var spriteFile;
-var cssFile;
-
-fs.readFile('./images/awAchSprite.jpg', function (err, data) {
-  if (err) {
-    throw err;
-  }
-  spriteFile = data;
-});
-
-fs.readFile('./styles/main.css', function (err, data) {
-  if (err) {
-    throw err;
-  }
-  cssFile = data;
-});
-
-//Create a server
-var myServer = http.createServer(function (request, response) {
-  // Return something from server
-
-  switch (request.url) {
-    case "/styles/main.css":
-      response.writeHead(200, { "Content-Type": "text/css" });
-      response.end(cssFile);
-      break;
-    case "/images/awAchSprite.jpg":
-      if (!response.getHeader('Cache-Control') || !response.getHeader('Expires')) {
-        response.setHeader("Cache-Control", "public, max-age=345600"); // ex. 4 days in seconds.
-        response.setHeader("Expires", new Date(Date.now() + 345600000).toUTCString());  // in ms.
-      }
-      response.writeHead(200, { "Content-Type": "image/jpeg" });
-      response.end(spriteFile);
-      break;
-    default:
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write('<link rel="stylesheet" type="text/css" href="/styles/main.css">');
-      const { awards, achievements, allAwards, allAchievements } = genRandBoard();
-      response.write('<div class = "container">');
-
-      achievements.forEach(ach =>
-        response.write(`<div class = "icon icon-${ach}"></div>`));
-      response.write('</div><div class = "container">');
-      awards.forEach(aw =>
-        response.write(`<div class = "icon icon-${aw}"></div>`));
-
-      response.write('</div>');
-      /*if(false) {
-        response.write('<br>');
-        response.write(`${allAwards+""}`)
-        response.write('<br>');
-        response.write(`${allAchievements+""}`) 
-      }*/
-      response.end();
-  };
-}); //create a server
-
 Array.prototype.getnkill = function () {
   var a = Math.floor(Math.random() * this.length);
   var dead = this[a];
@@ -65,7 +5,7 @@ Array.prototype.getnkill = function () {
   return dead;
 }
 
-function genRandBoard() {
+function genRandAwAch() {
   let allAchievements = Array.from({ length: 16 }, (x, i) => i);
   allAchievements.splice(12, 1);
   let allAwards = Array.from({ length: 12 }, (x, i) => i + 16);
@@ -79,9 +19,170 @@ function genRandBoard() {
     awards.push(allAwards.getnkill());
     achievements.push(allAchievements.getnkill());
   }
-  return { awards, achievements, allAwards, allAchievements };
+  return { awards, achievements };
 }
 
-var port = process.env.PORT || 3000
+function genRandBoard() {
 
-myServer.listen(port);
+  const { awards, achievements } = genRandAwAch();
+
+  let allTiles = Array.from({ length: 61 }, (x, i) => i);
+  const resources = Array.from({ length: 61 }, (x, i) => []);
+
+  allTiles.forEach(i => {
+    if (Math.floor(Math.random() * 1.9)) {
+      resources[i].push(Math.floor(Math.random() * 5));
+      if (Math.floor(Math.random() * 1.7)) {
+        if (Math.floor(Math.random() * 1.15)) {
+          resources[i].push(Math.floor(Math.random() * 5));
+        }
+        else {
+          resources[i].push(resources[i][0]);
+          if (Math.floor(Math.random() * 1.05)) {
+            resources[i].push(resources[i][0]);
+          }
+        }
+      }
+    }
+
+  });
+
+  const oceanTiles = [];
+  for (let i = 0; i < 12; i++) {
+    oceanTiles.push(allTiles.getnkill());
+  }
+
+  return { oceanTiles, resources };
+}
+
+$(document).ready(() => {
+  const { awards, achievements } = genRandAwAch();
+
+  $awContainer = $('#awContainer');
+  $achContainer = $('#achContainer');
+
+  $boardContainer = $('#boardContainer');
+
+  if ($awContainer.length && $achContainer.length) {
+
+    achievements.forEach(ach =>
+      $achContainer.append(`<div class = "icon icon-${ach}"></div>`));
+
+    awards.forEach(aw =>
+      $awContainer.append(`<div class = "icon icon-${aw}"></div>`));
+  }
+
+  if ($boardContainer.length) {
+    const { oceanTiles, resources } = genRandBoard();
+    // console.log(oceanTiles, resources);
+
+    var canvas = document.getElementById('boardCanvas');
+    canvas.setAttribute('width', '2049');
+    canvas.setAttribute('height', '1734')
+    var ctx = canvas.getContext('2d');
+
+    const loadedImgs = new Map();
+    const BOARDIMGSRC = "/images/board.jpg";
+    const OCNIMGSRC = "/images/oceanTile.png";
+    const RESIMGSRC = "/images/resSprite.png";
+    const AWACHIMGSRC = "/images/awAchSprite.png";
+
+    [BOARDIMGSRC, OCNIMGSRC, RESIMGSRC, AWACHIMGSRC].forEach((imgsrc, i, arr) => {
+      let img = new Image();
+      img.addEventListener('load', function () {
+        onImgLoad(img, imgsrc, arr.length);
+      }, false);
+      img.src = imgsrc;
+    });
+
+    function getTilePos(index) {
+      let row;
+      let width;
+      let rowPos;
+
+      rowPos = index + 1;
+      row = 0;
+      while (row < 10) {
+        width = 9 - Math.abs(row - 4);
+        if (rowPos <= width) {
+          rowPos--;
+          break;
+        }
+        rowPos -= width;
+        row++;
+      }
+
+      return {
+        row,
+        rowPos,
+        x: 686 + 136 * rowPos - 68 * (4 - (Math.abs(4 - row))),
+        y: 313.5 + 118 * row
+      };
+    }
+
+    function onImgLoad(img, index, imgNum) {
+      loadedImgs.set(index, img);
+      if (loadedImgs.size !== imgNum) {
+        return;
+      }
+      ctx.drawImage(loadedImgs.get(BOARDIMGSRC), 0, 0);
+      ctx.globalAlpha = 1;
+      let row;
+      let width;
+      let rowPos;
+
+      oceanTiles.forEach(i => {
+        ({ x, y } = getTilePos(i));
+        ctx.drawImage(loadedImgs.get(OCNIMGSRC), x, y);
+      });
+
+      resources.forEach((res, i) => {
+        if (!res.length) {
+          return;
+        }
+
+        ({ x, y } = getTilePos(i));
+        switch (res.length) {
+          case 1:
+            ctx.drawImage(loadedImgs.get(RESIMGSRC),
+              res[0] * 53, 0, 53, 67,
+              x + 50, y + 25, 26, 33);
+            break;
+          case 2:
+          ctx.drawImage(loadedImgs.get(RESIMGSRC),
+          res[0] * 53, 0, 53, 67,
+          x + 50, y + 25, 26, 33);
+          ctx.drawImage(loadedImgs.get(RESIMGSRC),
+          res[1] * 53, 0, 53, 67,
+          x + 25, y + 50, 26, 33);
+            break;
+          case 3:
+          ctx.drawImage(loadedImgs.get(RESIMGSRC),
+          res[0] * 53, 0, 53, 67,
+          x + 25, y + 50, 26, 33);
+          ctx.drawImage(loadedImgs.get(RESIMGSRC),
+          res[1] * 53, 0, 53, 67,
+          x + 55, y + 50, 26, 33);
+          ctx.drawImage(loadedImgs.get(RESIMGSRC),
+          res[2] * 53, 0, 53, 67,
+          x + 85, y + 50, 26, 33);
+            break;
+        }
+
+      });
+      
+      awards.forEach((i, ind) => {
+        ctx.drawImage(loadedImgs.get(AWACHIMGSRC),
+        40.5 + (i % 4)*289, 28 + Math.floor(i / 4)*168.75, 289, 168.75,
+          ind*179  + 119, 1545, 169, 99);
+      });
+
+      achievements.forEach((i, ind) => {
+        ctx.drawImage(loadedImgs.get(AWACHIMGSRC),
+        40.5 + (i % 4)*289, 28 + Math.floor(i / 4)*168.75, 289, 168.75,
+          ind*176  + 1049, 1537, 176, 104);
+      });
+
+    }
+  }
+});
